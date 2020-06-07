5eb9b9063f4fe66aacb9670882599b5f433058c9
Stryker Stinnette <strykerkent@gmail.com>
2020-06-07 16:30:08 -0500

    initial commit. added files for github pages.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      cards: [
+        {
+          id: 1,
+          name: 'Steve'
+        },
+        {
+          id: 2,
+          name: 'Bob'
+        },
+        {
+          id: 3,
+          name: 'Tom'
+        },
+      ]
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        {
+          this.state.cards.map((cards) => <h2 key={cards.id}>{cards.name}</h2>)
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
